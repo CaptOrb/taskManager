5270b727fad2b64227ff7344140ffa44f22e28da
CaptOrb <tehorb13@gmail.com>
2024-11-04 15:56:47 +0000

    add cors header for prod

--- backend/src/main/java/com/conor/taskmanager/security/SecurityConfig.java
+++ backend/src/main/java/com/conor/taskmanager/security/SecurityConfig.java
@@ -48,6 +48,7 @@ public class SecurityConfig {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.addAllowedOrigin("http://localhost");
         configuration.addAllowedOrigin("http://localhost:3000");
+        configuration.addAllowedOrigin("https://qube.app.librepush.net");
         configuration.addAllowedMethod("*");
         configuration.addAllowedHeader("*");
         configuration.setAllowCredentials(true);
