f0cd56143420894179964a110e0b7228af2083b2
CaptOrb <tehorb13@gmail.com>
2024-11-11 22:40:38 +0000

    Fix textarea workwrapping

--- frontend/src/components/CreateTask.jsx
+++ frontend/src/components/CreateTask.jsx
@@ -83,7 +83,7 @@ const CreateTask = () => {
             ></textarea>
 
             <h4 className="mt-4 text-lg font-semibold">Preview:</h4>
-            <div className="p-4 bg-gray-100 border rounded-lg dark:bg-gray-800 dark:text-white">
+            <div className="p-4 bg-gray-100 border rounded-lg dark:bg-gray-800 dark:text-white" wrap="hard">
                 <ReactMarkdown>{description}</ReactMarkdown>
             </div>
         </div>
