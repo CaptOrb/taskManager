9ecabbcc786a47024b0d70416901a67eed182f85
CaptOrb <tehorb13@gmail.com>
2025-02-12 14:40:26 +0000

    fix userrepo

--- backend/src/main/java/com/conor/taskmanager/repository/UserRepository.java
+++ backend/src/main/java/com/conor/taskmanager/repository/UserRepository.java
@@ -2,9 +2,11 @@ package com.conor.taskmanager.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import com.conor.taskmanager.model.User;
 
+@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
 	@Query("SELECT u FROM User u WHERE u.email = ?1")
