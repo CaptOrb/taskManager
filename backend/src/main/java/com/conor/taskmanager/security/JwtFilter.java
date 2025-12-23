65a5c6c3abfd661ecc74c11532ff1661cd77a3a2
Conor <tehorb13@gmail.com>
2025-12-23 03:04:08 +0000

    Upgrades backend to Spring Boot 4

--- backend/src/main/java/com/conor/taskmanager/security/JwtFilter.java
+++ backend/src/main/java/com/conor/taskmanager/security/JwtFilter.java
@@ -8,7 +8,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.lang.NonNull;
+import org.jspecify.annotations.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
